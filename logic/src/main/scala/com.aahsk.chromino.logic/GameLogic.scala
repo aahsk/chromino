4661ce384bc393cab8b02a778c171c1d5820c31e
Krisjanis Veinbahs <krisjanis.veinbahs@gmail.com>
2020-12-19 19:09:26 +0200

    Split board chromino validation into composable functions

--- logic/src/main/scala/com.aahsk.chromino.logic/GameLogic.scala
+++ logic/src/main/scala/com.aahsk.chromino.logic/GameLogic.scala
@@ -1,17 +1,17 @@
 package com.aahsk.chromino.logic
 
 import com.aahsk.chromino.domain._
+
 import scala.util.Random
 
 object GameLogic {
-  def validateBoardChromino(
+  def validateNoOverlap(
     pieces: List[BoardChromino],
     addedPiece: BoardChromino
-  ): Either[String, BoardChromino] = {
+  ): Option[String] = {
     val existingSquares = pieces.flatMap(_.squared())
     val addedSquares    = addedPiece.squared()
 
-    // Validate no overlap
     val isOverlapping = !addedSquares
       .forall(addedSquare =>
         existingSquares.forall(existingSquare =>
@@ -19,45 +19,54 @@ object GameLogic {
             addedSquare.position.y == existingSquare.position.y)
         )
       )
-    val maybeNoOverlapSquares = if (isOverlapping) {
-      Left("Chromino overlaps existing board chrominos")
+
+    if (isOverlapping) {
+      Some("Chromino overlaps existing board chrominos")
     } else {
-      Right(addedSquares)
+      None
     }
+  }
 
-    // Map every added square with its immediate neighbours
-    val maybeWithNeighbours: Either[String, List[(ChrominoSquare, List[ChrominoSquare])]] =
-      maybeNoOverlapSquares.flatMap(squares => {
-        val withNeighbours = squares.map(added =>
-          (
-            added,
-            existingSquares.filter(existing => {
-              val up =
-                existing.position.y == added.position.y + 1 && existing.position.x == added.position.x
-              val down =
-                existing.position.y == added.position.y - 1 && existing.position.x == added.position.x
-              val left =
-                existing.position.y == added.position.y && existing.position.x == added.position.x - 1
-              val right =
-                existing.position.y == added.position.y && existing.position.x == added.position.x + 1
+  def validateExistantNeighbourSquares(
+    pieces: List[BoardChromino],
+    addedPiece: BoardChromino
+  ): Either[String, List[(ChrominoSquare, List[ChrominoSquare])]] = {
+    val existingSquares = pieces.flatMap(_.squared())
+    val addedSquares    = addedPiece.squared()
 
-              up || down || left || right
-            })
-          )
-        )
+    val withNeighbours = addedSquares.map(added =>
+      (
+        added,
+        existingSquares.filter(existing => {
+          val up =
+            existing.position.y == added.position.y + 1 && existing.position.x == added.position.x
+          val down =
+            existing.position.y == added.position.y - 1 && existing.position.x == added.position.x
+          val left =
+            existing.position.y == added.position.y && existing.position.x == added.position.x - 1
+          val right =
+            existing.position.y == added.position.y && existing.position.x == added.position.x + 1
 
-        val allNeighbours = withNeighbours.flatMap { case (_, neighbours) => neighbours }
-        if (allNeighbours.isEmpty) {
-          Left("No neighbouring chrominos present")
-        } else if (allNeighbours.size < 2) {
-          Left("At least two neighbouring chrominos required")
-        } else {
-          Right(withNeighbours)
-        }
-      })
+          up || down || left || right
+        })
+      )
+    )
 
-    // Validate 2/3 squares touch only equal colors
-    maybeWithNeighbours
+    val allNeighbours = withNeighbours.flatMap { case (_, neighbours) => neighbours }
+    if (allNeighbours.isEmpty) {
+      Left("No neighbouring chrominos present")
+    } else if (allNeighbours.size < 2) {
+      Left("At least two neighbouring chrominos required")
+    } else {
+      Right(withNeighbours)
+    }
+  }
+
+  def validateWithMatchingNeighbours(
+    pieces: List[BoardChromino],
+    addedPiece: BoardChromino
+  ): Either[String, BoardChromino] = {
+    validateExistantNeighbourSquares(pieces, addedPiece)
       .flatMap(squares => {
         val isColorMatching = squares.forall { case (added, neighbours) =>
           neighbours.forall(neighbouring => {
@@ -74,6 +83,16 @@ object GameLogic {
           Left("Chromino colors don't match colors of nearby chrominos")
         }
       })
+  }
+
+  def validateBoardChromino(
+    pieces: List[BoardChromino],
+    addedPiece: BoardChromino
+  ): Either[String, BoardChromino] = {
+    validateNoOverlap(pieces, addedPiece) match {
+      case Some(error) => Left(error)
+      case None        => validateWithMatchingNeighbours(pieces, addedPiece)
+    }
   }
 
   def submitMove(
